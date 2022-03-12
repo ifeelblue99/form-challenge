d85a7b257787ad28151168ae53ddad14e98f498d
Baran Cakirbey <96394372+ifeelblue99@users.noreply.github.com>
2022-03-12 16:44:32 +0300

    Add files via upload

---
+++ FormChallenge/README.md
@@ -0,0 +1,23 @@
+Basic html, css and js form page created with the prerequisites below:
+
+Build a submission form using JS
+
+*Should have a name input that;
+
+*has min 6 chars,
+
+*has max 12 chars,
+
+*doesnt allow special chars
+
+*should have a feedback warning if doesn’t meet these requirements
+
+*Should have a gender input (might be radio buttons);
+
+*Should have a dropdown input in which the options are changed according to the gender above
+
+*Should have a date input which finally gives dd-mm-yyyy date format 
+
+*Should have a phone number input which has the exact   “0 555 555 55 55” format and have this format as you type(tip: use regex)
+
+*You can console.log all the inputs after submission
---
+++ FormChallenge/form-validation.js
@@ -0,0 +1,64 @@
+// DOM elements
+const nameInput = document.getElementById("name-input")
+const phoneInput = document.getElementById("phone-input")
+const productsSelector = document.getElementById("products")
+const dateInput = document.getElementById("date-input")
+const submitBtn = document.getElementById("submit-btn")
+
+// const vaild chars
+const specialChars = `/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;`
+const validNumbers = "0123456789"
+
+// validate name
+function validateName(name) {
+    
+    const isSpecialCharsPresent = specialChars
+        .split('').some(char =>name.includes(char)) 
+
+    return name.length>5&&name.length<13&&!isSpecialCharsPresent ? true : false 
+}
+// validate phone
+function validatePhone(phone) {
+    phone = phone.split("")
+    phone = phone.filter(el=>{
+        if(validNumbers.split("").includes(el)){
+            return el
+        }
+    })
+    return (
+        phone.length==11&&
+        phone[0]== 0&&
+        phone[1]!=0 ? true : false )
+}
+
+// when submit btn is clicked
+submitBtn.addEventListener("click", (event)=>{
+    event.preventDefault()
+
+    const isNameValid = validateName(nameInput.value)
+    const isphoneValid = validatePhone(phoneInput.value)
+
+    console.log()
+
+    if (isphoneValid && isNameValid) {
+        const gender = document.querySelector('input[name=gender]:checked').value
+        const selected = productsSelector.options[productsSelector.selectedIndex].value;
+        
+        
+        const formData = {
+            name:nameInput.value,
+            gender:gender,
+            prdouct:selected,
+            date:dateInput.value,
+            phone:phoneInput.value
+        }
+        alert("Form was sent successfuly. Check out your console log.")
+        console.log(formData)
+        nameInput.value = ""
+        phoneInput.value = ""
+        dateInput.value = ""
+    }
+    else{
+        alert("Something wrong happened with your data provided, please check your information.")
+    }
+})
